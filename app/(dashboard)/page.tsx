0a4c5e817c540dee3ce69de65c387375d99234ef
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-03-07 23:53:46 +0530

    build empty states

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,29 @@
-const Dashboard = () => {
+"use client"
+
+import { useOrganization } from "@clerk/nextjs"
+import { EmptyOrg } from "./_components/empty-org"
+import { BoardList } from "./_components/board-list";
+
+interface DashBoardPagesProps {
+    searchParams: {
+        search? :  string;
+        favorites? : string;
+    }
+}
+
+const Dashboard = ({searchParams}: DashBoardPagesProps) => {
+    const { organization } = useOrganization();  
     return (
-        <div>
-            DashBoard root page!!
+        <div className="flex-1 h-[calc(100%-80px)] p-6 ">
+            {!organization ? (
+                <EmptyOrg />
+            ) : (
+                <BoardList
+                    orgId = {organization.id}
+                    query = {searchParams}
+                />
+            )}
+            
         </div>
     )
 }
