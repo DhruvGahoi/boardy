311f2862817cef028df46994dd2e35289fd71fb4
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-03-07 19:54:36 +0530

    done with the navbar

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,21 @@
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+import { Button } from "@/components/ui/Button";
+import { Dialog, DialogContent, DialogTrigger } from "@radix-ui/react-dialog";
+
+export const InviteButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <Plus className="h-4 w-4 mr-2"/>
+                    Invite Members
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-transparent max-w-[880px] border-none">
+                <OrganizationProfile />
+            </DialogContent>
+        </Dialog>
+    )
+}
+
